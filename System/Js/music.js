a51bd94569845eb66d5ffb440b06bafddf56ab15
GladTHeMean <69305776+Githler001@users.noreply.github.com>
2023-12-21 00:52:54 +0700

    HMK's birthday V.3

---
+++ System/Js/music.js
@@ -0,0 +1,53 @@
+let music = document.getElementById("music");
+
+function playSong()
+{
+    if(music.paused)
+    {
+        music.play();
+        stopBtn();
+    }
+    else
+    {
+        music.pause();
+        playBtn();
+    }
+}
+
+let id = 0;
+
+function nextSong()
+{
+    if(id < 4)
+    {
+        id ++;
+        let nextMusic = document.getElementById(id);
+        music.src = nextMusic.src;
+        music.play();
+        stopBtn()
+    }
+    else
+    {
+        id = 0;
+        let nextMusic = document.getElementById(id);
+        music.src = nextMusic.src;
+        music.play();
+        stopBtn()
+    }
+}
+
+music.onended = function(){nextSong();}
+
+// Auguementative functions
+
+function playBtn()
+{
+    document.getElementById("smartBtnPlay").innerHTML='<img src="./System/img/svg/play.svg" alt="">';
+    document.getElementById("musicBox").innerHTML='<img src="./System/img/3TjN.png" class="music" alt="">';
+}
+
+function stopBtn()
+{
+    document.getElementById("smartBtnPlay").innerHTML='<img src="./System/img/svg/pause.svg" alt="">';
+    document.getElementById("musicBox").innerHTML='<img src="./System/img/3TjN.gif" class="music" alt="">';
+}
